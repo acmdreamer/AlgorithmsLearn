0e9a7cb6d64baa64a1066f56865e356b9636cad6
acmdreamer <1067882316@qq.com>
2017-12-09 23:17:12 +0800

    插入排序之折半查找法

--- ACM/src/main/java/com/cf/foundations/InsertSort.java
+++ ACM/src/main/java/com/cf/foundations/InsertSort.java
@@ -7,16 +7,17 @@ import java.util.List;
 
 /**
  * @Author: acmdreamer
- * @Description: 插入排序
+ * @Description: 插入排序之折半查找法
  * @Date: Create in 2017/12/9 18:45
  */
 public class InsertSort {
-    public static  void inderSort(List<Integer> list){
+    public static  void insertSort(List<Integer> list){
         int len = list.size();
         for(int sortIndex = 1; sortIndex < len;++sortIndex){
             int left = 0 , middle = 0 , right = sortIndex - 1;
             int key = list.get(sortIndex);
             if(key < list.get(sortIndex - 1)) {
+                //折半查找，指针最终必定定位于第一个大于key的数的前一个
                 while (left <= right) {
                     middle = (left + right) >> 1;
                     int cmpNum = list.get(middle);
@@ -26,9 +27,11 @@ public class InsertSort {
                         left = middle + 1;
                     }
                 }
+                //数组后移
                 for(int i = sortIndex - 1;i>right;--i){
                     list.set(i+1,list.get(i));
                 }
+                //插入
                 list.set(right+1,key);
             }
         }
@@ -36,7 +39,7 @@ public class InsertSort {
 
     public static void main(String[] args){
         List<Integer> list = MyUtil.getRandomIntegerArryayList(100,100);
-        inderSort(list);
+        insertSort(list);
         int len = list.size();
         for(int i=0;i<len;++i){
             System.out.print(list.get(i));
